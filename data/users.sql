d77d259c6f6f3335eb31baa000e7e68e75cd4cf1
odai1990 <odai22odai78@gmail.com>
2021-05-07 23:56:42 +0300

    login_sinup_top_tarck

---
+++ data/users.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS persons;
+ CREATE TABLE persons (
+   id SERIAL PRIMARY KEY,
+   name VARCHAR(255),
+   email VARCHAR(255),
+   password VARCHAR(255),
+   image VARCHAR(255) 
+ );
